3a2650c30e64c45e1b82f1793d31950645788238
kaanygit <yasinkaan345@hotmail.com>
2023-05-16 05:34:04 +0300

    generate completed
    application completed

--- src/components/section/section.components.jsx
+++ src/components/section/section.components.jsx
@@ -1,5 +1,4 @@
 import { Fragment, useEffect, useRef, useState } from "react"
-import SavePassword from "../save-password/save-password.component";
 
 
 const Section =()=>{
@@ -7,16 +6,22 @@ const Section =()=>{
     const str=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     const strGen=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     const symbol=['!','@','#','$','%','^','&','*','_','-','+','='];
-    let list=[];
+    
+    const rangeRef=useRef();
+    const checkboxRefs = [
+        useRef(),
+        useRef(),
+        useRef(),
+        useRef()
+    ];
 
-    const checkNum=useRef();
-    const checkStr=useRef();
-    const checkStrGen=useRef();
-    const checkSym=useRef();
-
-
+    const [activeItemId, setActiveItemId] = useState(null);
+    const [checkedItems, setCheckedItems] = useState([]);
     const [inputValue,setInputValue]=useState("");
-    const [passwordList,setPasswordList]=useState(()=>{
+    const [rangeInputBox,setRangeInputBox]=useState(10);
+
+    const [passwordList, setPasswordList] = useState([]);
+    const [passwordLocalStorage,setPasswordLocalStorage]=useState(()=>{
         const savedList=localStorage.getItem('passwordList');
         if(savedList){
             return JSON.parse(savedList);
@@ -25,91 +30,148 @@ const Section =()=>{
         }
     });
     
+    const handleCheckboxChange=()=>{
+        const checkedItems = checkboxRefs
+        .filter(ref => ref.current.checked)
+        .map(ref => ref.current.value);
+  
+      setCheckedItems(checkedItems);
+      console.log(checkedItems);
+    }
 
     const generatePassword=()=>{
         console.log('password generate');
-        handleChanceValue('deneme');
-        const checkEt= [checkNum, checkStr,checkStrGen,checkSym]
-        .filter(ref=>{
-            if(ref.current.checked === true ){
-                list = list.concat(ref.current.value);
-                return true
-            }else{
-                return false;
+        setPasswordList(passwordLocalStorage=>[...passwordLocalStorage,passwordList]);
+        const mergedList = checkedItems.join(',').split(',');
+        const randomItems=[];
+        for(let i=0;i<rangeInputBox;i++){
+            const randomIndex=Math.floor(Math.random()*mergedList.length);
+            const randomItem=mergedList[randomIndex];
+            randomItems.push(randomItem);
+            
+            if(!randomItems.includes(randomItem)){
+                randomItems.push(randomItem);
             }
-        });
-        console.log(list);
-        const liste=[...checkNum.current.value];
-        console.log(liste);
+        }
+        const result=randomItems.join('');
+        console.log(result);
+
+        const newPassword = {
+            id: Date.now(),
+            password: result
+          };
+          setPasswordLocalStorage(prevPasswordLocalStorage => [...prevPasswordLocalStorage, newPassword]);
+        
+
+        handleChanceValue(result);
     }
-    const handleChanceValue=(e)=>{
-        setInputValue(e);
+    const handleChanceValue=(event)=>{
+        setInputValue(event);
     }
     
-    // const savePassword=()=>{
-        
-    // }
+    const handleRangeChange = (e) => {
+        const value = parseInt(e.target.value);
+        setRangeInputBox(value);
+    };
+
+    const passwordCopy=(password)=>{
+        if(navigator.clipboard && navigator.clipboard.writeText){
+            navigator.clipboard.writeText(password)
+            .then(()=>{
+                console.log('kopyalandi',password);
+                setActiveItemId(null);
+            })
+            .catch((error)=>{
+                console.log('kopyalama hatası',password);
+            })
+        }else{
+            console.error('Panoya erişim desteklenmiyor')
+        }
+    };
+
+
+    const handleItemClick=(itemId)=>{
+        setActiveItemId(itemId);
+    };
+
+    const deleteLocalStorage=(id)=>{
+        setPasswordLocalStorage(prevPasswordLocalStorage => {
+            const updatedList = prevPasswordLocalStorage.filter(item => item.id !== id);
+            localStorage.setItem('passwordList', JSON.stringify(updatedList));
+            console.log('silme başarılı');
+            return updatedList;
+          });
+        setActiveItemId(null);
+    }
+
+
 
     useEffect(()=>{
-        if(inputValue){
-            localStorage.setItem('passwordList',JSON.stringify(inputValue));
-        }
-        console.log('');
-    },[inputValue])
+        if (passwordLocalStorage.length > 0) {
+            localStorage.setItem('passwordList', JSON.stringify(passwordLocalStorage));
+          }
+    },[passwordLocalStorage])
+
+    console.log(passwordLocalStorage);
 
 
     return(
         <Fragment>
-            <div className="passwordGenerator-container max-h-460px max-w-1500px border-2 p-5 rounded-md shadow-lg bg-white">
+            <div className="passwordGenerator-container max-h-468px max-w-1500px  border-2 p-5 rounded-md shadow-lg bg-white">
                 <div className="justify-center items-center text-center text-white font-bold text-3xl mb-3 pb-3 border-b border-gray-500">
                     <h1 className="text-sky-500">Password Generator</h1>
                 </div>
-                <div className="flex">
-                    <div className="password-generate-page mr-3">
+                <div className="flex h-36">
+                    <div className="password-generate-page mr-3 ">
                         <div>
                             <div>
-                                <input type="text"  onChange={handleChanceValue} value={inputValue}   placeholder="Password Generate" className="password-generator-input bg-gray-200 rounded-l-lg py-2 px-4" disabled/>
+                                <input type="text"  onChange={handleChanceValue} value={inputValue}  placeholder="Password Generate" className="password-generator-input bg-gray-200 rounded-l-lg py-2 px-4" disabled/>
                                 <button type="button" className="generate-button font-bold bg-sky-600 text-white hover:bg-sky-700 rounded-r-lg py-2 px-4 inline-flex" onClick={generatePassword}>Generate</button>
                             </div>
                             <div className="flex">
-                                <input type="range" value='50' className="password-length w-full"/>
-                                <h1 className="ml-3">8</h1>
+                                <input type="range" ref={rangeRef} defaultValue='10' onChange={handleRangeChange}  min='1' max='20'  className="password-length w-full"/>
+                                <h1 className="ml-3">{rangeInputBox}</h1>
                             </div>
                         </div>
                         <div className="checbox-page">
                             <div className="checkox-buttons grid grid-cols-2 gap-3">
                                 <div>
-                                    <input type="checkbox" ref={checkNum} value={number} key='numInput'/>
+                                    <input type="checkbox" ref={checkboxRefs[0]} value={number} onChange={handleCheckboxChange} key='numInput'/>
                                     <label>Numbers [ 1,2,3,... ]</label>
                                 </div>
                                 <div>
-                                    <input type="checkbox" ref={checkStr} value={str} key='alphaInput' />
+                                    <input type="checkbox" ref={checkboxRefs[1]} value={str} onChange={handleCheckboxChange} key='alphaInput' />
                                     <label>Alpha [ a,b,c,... ]</label>
                                 </div>
                                 <div>
-                                    <input type="checkbox" ref={checkStrGen} value={strGen} key='alphaGensInput'/>
+                                    <input type="checkbox" ref={checkboxRefs[2]} value={strGen} onChange={handleCheckboxChange} key='alphaGensInput'/>
                                     <label>AlphaGens [ A,B,C,... ]</label>
                                 </div>
                                 <div>
-                                    <input type="checkbox" ref={checkSym} value={symbol} key='symInput' />
+                                    <input type="checkbox" ref={checkboxRefs[3]} value={symbol} onChange={handleCheckboxChange} key='symInput' />
                                     <label>Symbols [ .,?,_,... ]</label>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="password-save-page bg-gray-300">
+                    <div className="password-save-page bg-gray-300 overflow-auto w-28 scrollbar-hide">
                         <div className="password-saves">
                             <div>
-                                <h1 className="bg-sky-500 fixed sticky">Password Saves</h1>
+                                <h1 className="bg-sky-500 sticky fixed text-white">Password Saves</h1>
                             </div>
                             <div className="saves-password text-center">
                                 <ul>
-                                    {/* {passwordList.map((passwordSave)=>(
-                                        <li key={1}>{passwordSave}</li>
-                                    ))} */}
-                                    {/* <li>Password 1</li>
-                                    <li>Password 2</li>  */}
-                                    <SavePassword/>
+                                    {passwordLocalStorage.map(passwordSave => (
+                                        <span key={passwordSave.id}>
+                                            <li className={`mt-3 cursor-pointer ${activeItemId === passwordSave.id ? 'active' : ''}`} onClick={() => handleItemClick(passwordSave.id)}>{passwordSave.password}</li>
+                                            {activeItemId === passwordSave.id && (
+                                                <span className="block bg-gray-500 flex">
+                                                    <button onClick={() => passwordCopy(passwordSave.password)} className="pr-1 bg-sky-500 hover:bg-sky-600 text-xs py-1 px-1 w-full">Copy</button>
+                                                    <button onClick={()=>deleteLocalStorage(passwordSave.id)} className="pl-1 bg-red-500 hover:bg-red-600 text-xs py-1 px-1 w-full ">Delete</button>
+                                                </span>
+                                            )}
+                                        </span>
+                                    ))}
                                 </ul>
                             </div>
                         </div>
